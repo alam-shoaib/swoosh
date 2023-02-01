cbd0e41b5dd4dc02b1f3fbfd8828071b0a8624f6
Shoaib <shoaib17coolalam@gmail.com>
2023-02-02 01:40:45 +0530

    all button funcnality added

--- app/src/main/java/com/example/swoosh/leaugeActivity.kt
+++ app/src/main/java/com/example/swoosh/leaugeActivity.kt
@@ -4,16 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_leauge.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class leaugeActivity : Log_activity() {
+    var selectLeauge=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leauge)
     }
+    fun menClick(view: View)
+    {
+        womenBtn.isChecked=false
+        coedBtn.isChecked=false
+        selectLeauge="mens"
+    }
+    fun womenClick(view: View)
+    {
+        menBtn.isChecked=false
+        coedBtn.isChecked=false
+        selectLeauge="women"
+    }
+    fun coedClick(view: View)
+    {
+        menBtn.isChecked=false
+        womenBtn.isChecked=false
+        selectLeauge="coed"
+    }
     fun nextClick(view:View)
     {
-        val nextpage= Intent(this,skill_activity::class.java)
-        startActivity(nextpage)
+        if(selectLeauge!="") {
+            val nextpage = Intent(this, skill_activity::class.java)
+            nextpage.putExtra(EXTRA_LEAUGE,selectLeauge)
+            startActivity(nextpage)
+        }
+        else
+        {
+            Toast.makeText(this,"select any of the above leauge",Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/swoosh/skill_activity.kt
+++ app/src/main/java/com/example/swoosh/skill_activity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class skill_activity : AppCompatActivity() {
+    var leauge=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        leauge= intent.getStringExtra(EXTRA_LEAUGE).toString()
+        println(leauge)
         setContentView(R.layout.activity_skill)
     }
 }
