5c6d62e766711fcc153429a764d628982efe321b
Shoaib <shoaib17coolalam@gmail.com>
2023-02-02 01:07:53 +0530

    error fixed

--- app/src/main/java/com/example/swoosh/leaugeActivity.kt
+++ app/src/main/java/com/example/swoosh/leaugeActivity.kt
@@ -1,12 +1,19 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class leaugeActivity : Log_activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leauge)
+    }
+    fun nextClick(view:View)
+    {
+        val nextpage= Intent(this,skill_activity::class.java)
+        startActivity(nextpage)
     }
 }
